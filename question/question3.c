c271e4319c729fabb6c4aaedf4a79c46ba73ca8e
davidlei <davidleitw@gmail.com>
2021-09-03 01:22:41 +0800

    question3.c 待補充驗證排序測試

--- question/question3.c
+++ question/question3.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 
-
 typedef struct __list {
     int data;
     struct __list *next;
 } list;
+
+void print(list *head) {
+    list *tmp = head;
+    while(tmp) {
+        printf("%d ", tmp->data);
+        tmp = tmp->next;
+    }
+    printf("\n");
+}
 
 list *sort(list *start) {
     if (!start || !start->next) // return single node, or NULL;
@@ -14,16 +21,16 @@ list *sort(list *start) {
     list *left = start;
     list *right = left->next;
     left->next = NULL;
-    /* 
-        LL0; 
-        left->next = NULL:
-    */
-
+    /* LL0; */
     left = sort(left);
     right = sort(right);
+    print(left);
+    print(right);
 
-    // recursion insertion sort, insert left element to sorted right list. 
     for(list *merge = NULL; left || right; ) {
+        /*
+            right 是已經排列好的, 如果 left->data < right->data, 則應該要把 left 當作新的 head;.
+        */
         if (!right || (left && left->data < right->data)) {
             if (!merge) {
                 start = merge = left;
@@ -36,6 +43,7 @@ list *sort(list *start) {
             left = left->next;
             /* LL3; */
         } else {
+            // left->data >= right->data    
             if (!merge) {
                 start = merge = right;
                 /* LL4; */
@@ -53,19 +61,23 @@ list *sort(list *start) {
 
 int main() {
     list *head = malloc(sizeof(list));
-    head->data = -10;
-
+    head->data = 1;
+ㄕ
     list *n1 = malloc(sizeof(list));
-    n1->data = 12;
+    n1->data = -3;
     head->next = n1;
 
     list *n2 = malloc(sizeof(list));
-    n2->data = -3;
+    n2->data = 2;
     n1->next = n2;
 
     list *n3 = malloc(sizeof(list));
-    n3->data = -18;
+    n3->data = -4;
     n2->next = n3;
+
+    list *n4 = malloc(sizeof(list));
+    n4->data = 5;
+    n3->next = n4;
 
     list *iter = head;
     while(iter) {
